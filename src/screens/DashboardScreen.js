1e603aaf93ea41ea7beeddc30cf21f26eaf7af15
Mrenard72 <maxime.renard72@gmail.com>
2025-02-25 16:42:12 +0100

    début

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image, ImageBackground } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const DashboardScreen = ({ navigation }) => {
+  
+  const handleLogout = async () => {
+    await AsyncStorage.removeItem('token'); // Supprime le token
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Login' }], // Redirige vers l'écran de connexion
+    });
+  };
+
   return (
     <ImageBackground source={require('../../assets/background.png')} style={styles.background}>
       <View style={styles.container}>
@@ -16,6 +26,11 @@ const DashboardScreen = ({ navigation }) => {
         <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('Discover')}>
           <Image source={require('../../assets/discover.jpg')} style={styles.cardImage} />
           <Text style={styles.cardText}>Je fais découvrir</Text>
+        </TouchableOpacity>
+
+        {/* Bouton Déconnexion */}
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Text style={styles.logoutButtonText}>Déconnexion</Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
@@ -66,6 +81,19 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     backgroundColor: 'rgba(0,0,0,0.5)',
     paddingVertical: 5,
+  },
+  logoutButton: {
+    backgroundColor: '#E53935', // Rouge
+    padding: 15,
+    borderRadius: 10,
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  logoutButtonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
